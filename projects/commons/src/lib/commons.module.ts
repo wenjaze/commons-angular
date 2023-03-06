6d8662d3077a0cfcd2d915666d3a64333e708e05
wenjaze <raska.gergely@gmail.com>
2023-03-06 18:43:24 +0100

    Add initial files

---
+++ projects/commons/src/lib/commons.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './components/button/button.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonComponent
+  ],
+  imports: [
+  ],
+  exports: [
+  ]
+})
+export class CommonsModule { }
---
+++ projects/commons/src/lib/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'commons-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+}
