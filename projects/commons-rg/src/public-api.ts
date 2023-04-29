78627327746e397ca6de887a83ab360de06543e2
wenjaze <raska.gergely@gmail.com>
2023-04-29 11:21:12 +0200

    Update export

--- projects/commons-rg/src/public-api.ts
+++ projects/commons-rg/src/public-api.ts
@@ -1,8 +1,6 @@
 /*
  * Public API Surface of commons
  */
-
-export * from './lib/commons.service';
 export * from './lib/components/button/button.component';
 export * from './lib/commons.module';
 
